d13aee7de2c7bb990749e92bcd551e76bda795be
Baktybek Tazhibaev <baktybektazhibaev@gmail.com>
2017-02-20 02:41:10 -0600

    Update day0.java

--- day0.java
+++ day0.java
@@ -1,4 +1,3 @@
-
 public class Solution {
 	public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
@@ -13,7 +12,6 @@ public class Solution {
       
 		// Print a string literal saying "Hello, World." to stdout.
 		System.out.println("Hello, World.");
-      
-	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
+        	System.out.println(inputString);     
 	}
 }
